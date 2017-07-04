ab1d594348e0ab9388bb341a7820b0215636c59b
Сергей Новоселов <Seg@MacBook-Air-Sergej.local>
2017-07-05 00:25:05 +0300

    Initial Commit

---
+++ Travel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by Sergey Novoselov on 05.07.17.
+//  Copyright © 2017 Sergey Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
